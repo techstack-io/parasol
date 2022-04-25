12da26d7608f736e5e3f334f845b2a6b9cf6915c
Dan <support@stackmetric.io>
2022-04-25 13:13:50 -0500

    Init Commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+    <Head>
+      <title>Parasol</title>
+      <meta name='description' content='A meditation app' />
+    </Head>
+    <h1>HELLO</h1>
+    </div>
+  )
+}
