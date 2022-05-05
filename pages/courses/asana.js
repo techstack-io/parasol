1d0e063c8ae1ac7458dd697febc75dbca4348dad
Dan <support@stackmetric.io>
2022-05-05 16:02:21 -0500

    reconfigured index page

---
+++ pages/courses/asana.js
@@ -0,0 +1,7 @@
+const Asana = () => {
+    return <div>
+        Welcome to your courses!
+    </div>
+};
+
+export default Asana;
--- pages/dojos.js
+++ pages/dojos.js
@@ -11,7 +11,6 @@ export async function getStaticProps(context) {
   };
 }
 
-
 export default function Dojos(props) {
   console.log('props', props);
     return(
@@ -19,6 +18,8 @@ export default function Dojos(props) {
       <PageHeading />
       <div className="bg-white">
         <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+          
+          <h1 className="text-2xl">Meditate in Chicago</h1>
           <div className="grid grid-cols-1 gap-y-16 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {dojos.map((dojo) => {
             return (
